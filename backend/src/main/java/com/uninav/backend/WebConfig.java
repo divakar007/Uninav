683fa0eb58d9a7062561aa5ab3af7f6ad2c6fd0b
Divakara Rao Annepu <annepudiva007@gmail.com>
2024-12-05 22:46:42 -0500

    UNINAV: changed cors policy for the deployment

--- backend/src/main/java/com/uninav/backend/WebConfig.java
+++ backend/src/main/java/com/uninav/backend/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
                     .allowedOrigins(
-                        "https://uninav.onrender.com/"
+                        "https://uninav.onrender.com/","http://localhost:8080"
                     )
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                     .allowedHeaders("*")
